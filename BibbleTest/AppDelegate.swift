8804c83ea95c364a3adde54e0dea422ab19c9cd4
Shinobu Izumi <stagesp1@gmail.com>
2017-11-02 00:38:54 +0900

    Initial Commit

---
+++ BibbleTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BibbleTest
+//
+//  Created by Shinobu Izumi on 2017/11/02.
+//  Copyright © 2017年 Shinobu Izumi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
